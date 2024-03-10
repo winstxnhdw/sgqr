d0aa4015fcdd8ee06b2910fb5bf344e6734d9238
winstxnhdw <winstxnhdw@gmail.com>
2024-03-10 09:51:22 +0800

    chore: clean up tests

--- tests/crc.test.ts
+++ tests/crc.test.ts
@@ -1,14 +1,7 @@
 import { crc16 } from '@/helpers'
 import { expect, test } from 'bun:test'
 
-test('returns the correct CRC-16 value for a given string', () => {
-  const input = 'hello world'
-  const result = crc16(input)
-  expect(result).toEqual('EFEB')
-})
+test('returns the correct CRC-16 value for a given string', () => expect(crc16('hello world')).toEqual('EFEB'))
 
-test('returns undefined if the input string contains a character with an undefined CRC-16 code', () => {
-  const input = '\u1000'
-  const result = crc16(input)
-  expect(result).toBeUndefined()
-})
+test('returns undefined if the input string contains a character with an undefined CRC-16 code', () =>
+  expect(crc16('\u1000')).toBeUndefined())

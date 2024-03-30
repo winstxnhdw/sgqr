47f94f5c9e4a82dce86bde1fb876e936ad5a1d05
winstxnhdw <winstxnhdw@gmail.com>
2024-03-31 05:19:59 +0800

    fix: properly target browser for `IIFE` build

---
+++ .changeset/gentle-zebras-leave.md
@@ -0,0 +1,5 @@
+---
+"sgqr": patch
+---
+
+fix IIFE distribution to target the browser
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,11 @@ export default defineConfig((options) => {
   return {
     entry: ['src/index.ts'],
     format: ['cjs', 'esm', 'iife'],
+    target: 'es2022',
+    platform: 'browser',
+    globalName: 'sgqr',
     minify: !options.watch,
+    treeshake: true,
     clean: true,
     dts: true,
   }

94852fc7d11f186daac0925235943227a207a7ec
winstxnhdw <winstxnhdw@gmail.com>
2024-07-03 22:38:46 +0100

    fix: UENs should support alphanumeric strings

---
+++ .changeset/bright-cherries-smile.md
@@ -0,0 +1,5 @@
+---
+"sgqr": patch
+---
+
+UENs should support alphanumeric
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import type {
+  Alphanumeric as AlphanumericGeneric,
   Amount as AmountGeneric,
   ExpiryDate as ExpiryDateGeneric,
   NumberString as NumberStringGeneric,
@@ -6,6 +7,7 @@ import type {
 
 export { generate, generate_code, generate_svg } from '@/generate'
 export type { CountryCode, CurrencyCode, DialingCode } from '@/types'
+export type Alphanumeric = AlphanumericGeneric<string>
 export type NumberString = NumberStringGeneric<string>
 export type Amount = AmountGeneric<string>
 export type ExpiryDate = ExpiryDateGeneric<string>
